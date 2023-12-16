7d7eb2404d7e68dac3df9e724bbfc4a30648a068
Greenie115 <dggreen545@gmail.com>
2023-12-16 18:38:49 +0000

    updated array of questions

--- index.js
+++ index.js
@@ -5,7 +5,14 @@ const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
 const questions = [
-
+    'What is the title of your project?',
+    'Describe the project',
+    'How does the user instal this project?',
+    'How would the user use this project?',
+    'Is there a license required? If so, which one?',
+    'How cna someone contribute to your codebase?',
+    'How would the user run tests on the project?',
+    'Is there anything else the user should know about the project?'
 ];
 
 // function to write README file
